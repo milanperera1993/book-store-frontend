6dc58fc377f86f5e093b5a79968c791950aab331
milanperera1993 <milanperera1993@gmail.com>
2025-01-03 17:14:39 +0800

    adding login and resgiter page UIs

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,26 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
     children: [
-       {
+      {
         path: "/",
-        element: <Home/>
-       },
-       {
+        element: <Home />,
+      },
+      {
         path: "/orders",
-        element : <div>Orders</div>
-       },
-       {
+        element: <div>Orders</div>,
+      },
+      {
         path: "/about",
-        element: <div>About</div>
-       }
-    ]
-  }, 
+        element: <div>About</div>,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
 ]);
 
 export default router;
