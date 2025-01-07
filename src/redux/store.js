c68d01e1193d4f6de590f70d337d154576579e54
milanperera1993 <milanperera1993@gmail.com>
2025-01-07 23:44:18 +0800

    book api with rtk queries and frontend data binding

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 // reducers
 import cartSlice from "./features/cart/cartSlice";
 
+// api
+import booksApi  from "./features/books/booksApi";
+
 export const store = configureStore({
   reducer: {
     cart: cartSlice,
+    [booksApi.reducerPath]: booksApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(booksApi.middleware),
 });
